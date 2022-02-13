bff468a359e2cbe0016677eac3d4c37ebe281cc3
JigSaW <misho_1998@mail.ru>
2022-02-13 23:59:29 +0300

    add

--- auth.py
+++ auth.py
@@ -1,6 +1,6 @@
 import telebot
 import sqlite3
-from PIL import Image
+from telebot import types
 
 # Создаем экземпляр бота
 bot = telebot.TeleBot('5115885214:AAF8KYpo7PAfTHUzC_iMjLv1b-MGicOd32o')
@@ -8,23 +8,36 @@ bot = telebot.TeleBot('5115885214:AAF8KYpo7PAfTHUzC_iMjLv1b-MGicOd32o')
 # Функция, обрабатывающая команду /start
 @bot.message_handler(commands=["start"])
 def start(message, res=False):
-    bot.send_message(message.chat.id, 'Добро пожаловать! Выберите личность:')
+    msg = bot.send_message(message.chat.id, 'Добро пожаловать!')
+    bot.register_next_step_handler(message, buttons(message))
+
+def buttons(message):
     conn = sqlite3.connect("bfm_db.db")
     cursor = conn.cursor()
     cursor.execute("""
-        SELECT 
-            name, 
-            surname 
-        FROM 
-            persons;
-    """)
+            SELECT 
+                name, 
+                surname 
+            FROM 
+                persons;
+        """)
     exists = cursor.fetchall()
+    keyboard = types.InlineKeyboardMarkup()
+    print(exists)
     for i in exists:
-        bot.send_message(message.chat.id, ' '.join(i))
+        keyboard.add(types.InlineKeyboardButton(text=f'{i[0]} {i[1]}', callback_data=f'{i[0]} {i[1]}'))
+    bot.send_message(message.chat.id, 'Выберите личность, '
+                                      'рекомендации которого вы хотели бы посмотреть:', reply_markup=keyboard)
     cursor.close()
 
-@bot.message_handler(content_types=["text"])
-def handle_text(message):
+def return_button(message):
+    keyboard = types.InlineKeyboardMarkup()
+    keyboard.add(types.InlineKeyboardButton(text=f'hgfjfgjhgf', callback_data=f'ghfjjghj'))
+    bot.send_message(message.chat.id, '', reply_markup=keyboard)
+
+@bot.callback_query_handler(func=lambda call: True)
+def query_handler(call):
+    print(call.data.split()[0])
     conn = sqlite3.connect("bfm_db.db")
     cursor = conn.cursor()
     cursor.execute(f"""
@@ -40,12 +53,14 @@ def handle_text(message):
         ON 
             p.id = b.person_id
         WHERE 
-            p.name = '{message.text.split()[0]}'
+            p.name = '{call.data.split()[0]}'
         AND
-           p.surname = '{message.text.split()[1]}';
+           p.surname = '{call.data.split()[1]}';
     """)
     exists = cursor.fetchall()
-    bot.send_message(message.chat.id, f'{exists[0][1]}\n{exists[0][2]}\n{exists[0][3]}')
+    bot.send_message(call.message.chat.id, f'{call.data} рекомендует:')
+    for desc in exists:
+        bot.send_message(call.message.chat.id, f'*{desc[1]}\n{desc[2]}*\n[Ссылка]({desc[3]})', parse_mode='Markdown')
     cursor.close()
 
 # Запускаем бота
