a11159b0a998182737bd279984747347d2760cd5
JigSaW <misho_1998@mail.ru>
2022-02-16 23:40:27 +0300

    update func

--- auth.py
+++ auth.py
@@ -9,9 +9,16 @@ bot = telebot.TeleBot('5115885214:AAF8KYpo7PAfTHUzC_iMjLv1b-MGicOd32o')
 @bot.message_handler(commands=["start"])
 def start(message, res=False):
     msg = bot.send_message(message.chat.id, 'Добро пожаловать!')
-    bot.register_next_step_handler(message, buttons(message))
+    buttons(message)
+
+@bot.message_handler(content_types=["text"])
+def handle_text(message):
+    print(type(message))
+    msg = bot.send_message(message.chat.id, 'Управление через кнопки!')
+    buttons(message)
 
 def buttons(message):
+    print('aaaaaaaaaaaaaa')
     conn = sqlite3.connect("bfm_db.db")
     cursor = conn.cursor()
     cursor.execute("""
@@ -23,46 +30,48 @@ def buttons(message):
         """)
     exists = cursor.fetchall()
     keyboard = types.InlineKeyboardMarkup()
-    print(exists)
     for i in exists:
         keyboard.add(types.InlineKeyboardButton(text=f'{i[0]} {i[1]}', callback_data=f'{i[0]} {i[1]}'))
     bot.send_message(message.chat.id, 'Выберите личность, '
-                                      'рекомендации которого вы хотели бы посмотреть:', reply_markup=keyboard)
+                                        'рекомендации которого вы хотели бы посмотреть:', reply_markup=keyboard)
     cursor.close()
 
 def return_button(message):
     keyboard = types.InlineKeyboardMarkup()
-    keyboard.add(types.InlineKeyboardButton(text=f'hgfjfgjhgf', callback_data=f'ghfjjghj'))
-    bot.send_message(message.chat.id, 'ergdergdr', reply_markup=keyboard)
+    keyboard.add(types.InlineKeyboardButton(text=f'Выбрать другую личность', callback_data='select'))
+    bot.send_message(message.chat.id, "Выбрать другую личность?", reply_markup=keyboard)
 
 @bot.callback_query_handler(func=lambda call: True)
 def query_handler(call):
-    print(call.data.split()[0])
-    conn = sqlite3.connect("bfm_db.db")
-    cursor = conn.cursor()
-    cursor.execute(f"""
-        SELECT 
-            b.photo, 
-            b.author, 
-            b.title, 
-            b.link
-        FROM 
-            books AS b
-        JOIN 
-            persons AS p
-        ON 
-            p.id = b.person_id
-        WHERE 
-            p.name = '{call.data.split()[0]}'
-        AND
-           p.surname = '{call.data.split()[1]}';
-    """)
-    exists = cursor.fetchall()
-    bot.send_message(call.message.chat.id, f'{call.data} рекомендует:')
-    for desc in exists:
-        bot.send_message(call.message.chat.id, f'*{desc[1]}\n{desc[2]}*\n[Ссылка]({desc[3]})', parse_mode='Markdown')
-    cursor.close()
-    bot.register_next_step_handler(call.message, return_button(call.message))
+    if call.data != "select":
+        conn = sqlite3.connect("bfm_db.db")
+        cursor = conn.cursor()
+        cursor.execute(f"""
+                        SELECT 
+                            b.photo, 
+                            b.author, 
+                            b.title, 
+                            b.link
+                        FROM 
+                            books AS b
+                        JOIN 
+                            persons AS p
+                        ON 
+                            p.id = b.person_id
+                        WHERE 
+                            p.name = '{call.data.split()[0]}'
+                        AND
+                            p.surname = '{call.data.split()[1]}';
+                    """)
+        exists = cursor.fetchall()
+        bot.send_message(call.message.chat.id, f'{call.data} рекомендует:')
+        for desc in exists:
+            bot.send_message(call.message.chat.id, f'*{desc[1]}\n{desc[2]}*\n[Ссылка]({desc[3]})',
+                            parse_mode='Markdown')
+        cursor.close()
+        return_button(call.message)
+    else:
+        buttons(call.message)
 
 # Запускаем бота
 bot.polling(none_stop=True, interval=0)
